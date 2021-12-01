dfae93ffdecc4f6036a521a2dfd1c8ce08a47860
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-12-01 13:17:22 +0530

    UC5: Displayed Hello From Bridgelabz With User Input Name.

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,23 @@ import './App.css';
 import logo from './assets/logo.png';
 
 const App = () => {
-  const [title, setTitle] = useState("Hello World, From Bridgelabz");
+  const [title, setTitle] = useState("World");
 
   const openBridgelabz = () => {
     window.open("https://www.bridgelabz.com/");
   }
 
+  const nameChange = (event) => {
+    setTitle(event.target.value);
+  }
+
   return (
     <div className="App">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">Hello {title}, From Bridgelabz</h1>
       <img className="logo" src={logo} alt="Bridgelabz Logo" onClick={openBridgelabz} target="_blank" />
+      <div className="inputBox">
+        <input className="input" placeholder="Enter Your Name" type="text" onChange={nameChange} />
+      </div>
     </div>
   );
 }
