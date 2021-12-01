31b6ea1bd6b90ddab39f1ff9253266125a0a5729
Shaikh Afzal <xfzxlshxikh@gmail.com>
2021-12-01 12:19:51 +0530

    UC3: Added BridgeLabz Logo.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
 
 import './App.css';
+import logo from './assets/logo.png';
 
 const App = () => {
   const [title, setTitle] = useState("Hello World, From Bridgelabz")
 
   return (
-    <h1>{title}</h1>
+    <div className="App">
+      <h1 className="title">{title}</h1>
+      <img className="logo" src={logo} alt="Bridgelabz Logo" />
+    </div>
   );
 }
 
